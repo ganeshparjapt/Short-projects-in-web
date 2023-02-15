938124b2a1957a3c87e99602cfb1e1ca12d153e4
ganesh <ganeshparjaptm5@gmail.com>
2023-02-15 18:47:29 +0530

    add some basic fitures

--- Switchable Form/scrpt.js
+++ Switchable Form/scrpt.js
@@ -1,18 +1,17 @@
-function openForm(evt,formName){
-    document.getElementsByClassName(switchcontent).style.display= "block";
-    evt.currentTarget.className += "active";
-}
-//declare all variables
-let i, switchcontent,switchlinks;
+/* function to open form based on formName */
+function openForm(evt, formName) {
+    document.getElementById(formName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 
-//get all enelmnts with class
-switchcontent=document.getElementsByClassName("switchcontent");
-for(i = 0;i < switchcontent.length;i++){
+  let i,switchcontent, switchlinks;
+
+  switchcontent=document.getElementsByClassName("switchcontent");
+  for(i=0;i<switchcontent.length;i++){
     switchcontent[i].style.display="none";
-}
 
-//get all button elements with class ="switchlink" and remove the class "active "for loop
-switchlinks=document.getElementsByClassName("switchlink");
-for(i=0;i<switchlinks.length;i++){
-    switchlinks[i].className=switchlinks[i].className.repeat("active","")
-;}
+    switchlinks=document.getElementsByClassName("switchlink");
+    for(i=0;i<switchlinks.length;i++){
+        switchlinks[i].className=switchlinks[i].className.repeat("active");
+    }
+  }
