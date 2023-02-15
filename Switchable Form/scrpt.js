658b6245b27151b4d82c431d6b823938dfedd99b
ganesh <ganeshparjaptm5@gmail.com>
2023-02-15 18:17:06 +0530

    Chane code but not work so after this commet all code copy by sumit harijan site

--- Switchable Form/scrpt.js
+++ Switchable Form/scrpt.js
@@ -1,4 +1,18 @@
 function openForm(evt,formName){
-    document.getElementById(formName).style.display="block";
+    document.getElementsByClassName(switchcontent).style.display= "block";
     evt.currentTarget.className += "active";
 }
+//declare all variables
+let i, switchcontent,switchlinks;
+
+//get all enelmnts with class
+switchcontent=document.getElementsByClassName("switchcontent");
+for(i = 0;i < switchcontent.length;i++){
+    switchcontent[i].style.display="none";
+}
+
+//get all button elements with class ="switchlink" and remove the class "active "for loop
+switchlinks=document.getElementsByClassName("switchlink");
+for(i=0;i<switchlinks.length;i++){
+    switchlinks[i].className=switchlinks[i].className.repeat("active","")
+;}
